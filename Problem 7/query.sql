ea877eb6a0731e0462f7ff8c2dac726608814087
Victor Lai <victorlai@Victors-MacBook-Pro.local>
2023-04-18 22:06:12 +0800

    Updated Query

--- Problem 7/query.sql
+++ Problem 7/query.sql
@@ -3,4 +3,4 @@ FROM balances b /* starting from the balances table */
 JOIN trades t ON b.address = t.address /* combine the two tables */
 WHERE t.block_height > 730000 /* recent trade made */
 GROUP BY b.address /* filter wallet balance */
-HAVING SUM(CASE WHEN b.denom = 'usdc' THEN b.amount / 1000000 WHEN b.denom = 'swth' THEN b.amount * 0.00000005 WHEN b.denom = 'tmz' THEN b.amount * 0.003 END) >= 500;
+HAVING SUM(CASE WHEN b.denom = 'usdc' THEN b.amount * 0.000001 WHEN b.denom = 'swth' THEN b.amount * 0.00000005 WHEN b.denom = 'tmz' THEN b.amount * 0.003 END) >= 500;
